beac34a93cd3f7a19873e11eba7960a7ca13c1c9
Team 811 <team811cardinals@gmail.com>
2022-02-09 20:50:58 -0500

    ultrasonic is back - crane

---
+++ src/main/java/frc/robot/Vision/Ultrasonic.java
@@ -0,0 +1,18 @@
+package frc.robot.Vision;
+
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Ultrasonic extends TimedRobot{
+    private final AnalogInput ultrasonic = new AnalogInput(0);
+
+    public double getDistance() {
+        double rawValue = ultrasonic.getValue();
+        double voltage_scale_factor = 5/RobotController.getVoltage5V();
+        double currentDistanceCentimeters = rawValue * voltage_scale_factor * 0.125;
+        return currentDistanceCentimeters;
+    }
+
+}
