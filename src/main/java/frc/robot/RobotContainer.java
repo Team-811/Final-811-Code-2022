930714c95b21b700ba03fcb094ab672db62e7d92
Team 811 <team811cardinals@gmail.com>
2022-02-09 20:38:36 -0500

    prelim auto code which is just drive forward, not based on vision - crane

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -86,6 +86,8 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
+    boolean autoMode = true;
+    
     // An ExampleCommand will run in autonomous
     return m_autoCommand;
   }
