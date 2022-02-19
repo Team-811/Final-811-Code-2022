6ba7a7eebfa49318245d262f5f2a117d9eb42007
Team 811 <team811cardinals@gmail.com>
2022-02-19 14:06:20 -0500

    qf1v2

---
+++ src/main/java/frc/robot/commands/Shooter/SlowShooter.java
@@ -0,0 +1,32 @@
+package frc.robot.commands.Shooter;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Shooter;
+
+public class SlowShooter extends CommandBase {
+   private Shooter requiredSubsystem;
+
+   public SlowShooter(Shooter m_SubsystemBase) {
+       requiredSubsystem = m_SubsystemBase;
+       addRequirements(requiredSubsystem);
+   }
+ 
+   @Override
+   public void initialize() {}
+ 
+   @Override
+   public void execute() {
+      double distance = GetDistance.Distance();
+      double SHOOTER_SPEED = distance * 0.1; // placeholder 
+      requiredSubsystem.shooterSpin((Constants.SHOOTER_SPEED / 2) + 0.05);
+}
+ 
+   @Override
+   public void end(boolean interrupted) {}
+ 
+   @Override
+   public boolean isFinished() {
+     return true;
+   }
+ }
