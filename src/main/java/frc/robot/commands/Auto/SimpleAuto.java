65e51f1ef40b84d39d80599c5bd540fc7ed0a9dc
Team 811 <team811cardinals@gmail.com>
2022-02-20 13:26:26 -0500

    simple auto from week 0 - crane

---
+++ src/main/java/frc/robot/commands/Auto/SimpleAuto.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Auto;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Drivetrain;
+
+public class SimpleAuto extends ParallelDeadlineGroup {
+  public SimpleAuto(Drivetrain subsystem) {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+
+    super(new WaitCommand(0), new InstantCommand(()->subsystem.driveForward(0.7), subsystem));
+
+
+  }
+
+}
