47b31743e9f4a5e1f083d7ceff3c50440c4dd771
alex <alexb_sb@yahoo.com>
2022-02-24 17:45:45 -0500

    Preliminary PID climber arm code - Alex

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -19,6 +19,8 @@ public class RobotMap {
     public static final int INTAKE_PISTON_EXTENTION = 6;
     public static final int INTAKE_PISTON_RETRACTION = 7;
 
+    public static final int CLIMBER_WINCH_LEFT = 14; //might be switched around
+    public static final int CLIMBER_WINCH_RIGHT = 15;
     public static final int CLIMBER_ARM_LEFT = 18; //might be switched around 
     public static final int CLIMBER_ARM_RIGHT = 19;
 
